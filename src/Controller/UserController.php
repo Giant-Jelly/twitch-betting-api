81a5fc0c3c227249383d841289cb939c3aa739d8
giantjelly <nwalker@rawnet.com>
2019-03-18 14:54:24 -0300

    added register test

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/user", name="ApiUser")
+ *
+ * Class UserController
+ * @package App\Controller
+ */
+class UserController extends BaseController
+{
+    /**
+     * @Route("/register", name="Register", methods={"POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function register(Request $request): Response
+    {
+        return new Response('test');
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $credits;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return User
+     */
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCredits(): ?int
+    {
+        return $this->credits;
+    }
+
+    /**
+     * @param int $credits
+     * @return User
+     */
+    public function setCredits(int $credits): self
+    {
+        $this->credits = $credits;
+
+        return $this;
+    }
+}
