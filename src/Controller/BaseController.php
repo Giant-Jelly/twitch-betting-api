9a465736316d685294a3da7eedb4bef048e26d8d
giantjelly <nwalker@rawnet.com>
2019-03-18 15:25:29 -0300

    test endpoint

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,13 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
+ * @Route("/api", name="ApiTest")
+ *
  * Class BaseController
  * @package App\Controller
  */
 class BaseController extends AbstractController
 {
-
+    /**
+     * @Route("/test", name="Test", methods={"GET"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function test(Request $request): Response
+    {
+        return new Response($request->headers->get('Nightbot-User')['name']);
+    }
 }
