143397ab23850d3c9c6718485b022f39dffe81e5
giantjelly <nwalker@rawnet.com>
2019-03-23 19:19:53 -0300

    header fix

--- src/Response/JsonApiResponse.php
+++ src/Response/JsonApiResponse.php
@@ -16,7 +16,7 @@ class JsonApiResponse extends JsonResponse
         $headers = [
             'Access-Control-Allow-Origin' => '*',
             'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS',
-            'Access-Control-Allow-Headers' => 'authorization,content-type,x-json'
+            'Access-Control-Allow-Headers' => 'authorization,content-type,gj-json'
         ];
         parent::__construct($content, $code, $headers);
     }
