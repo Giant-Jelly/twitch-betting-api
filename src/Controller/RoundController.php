ed09f47542ace5d8052fc4410043c3179e4d44e1
giantjelly <nwalker@rawnet.com>
2019-03-23 19:25:05 -0300

    header fix

--- src/Controller/RoundController.php
+++ src/Controller/RoundController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/round", name="ApiRound")
+ * @Route("/api/round", name="ApiRound", methods={"GET", "OPTIONS"})
  *
  * Class RoundController
  * @package App\Controller
